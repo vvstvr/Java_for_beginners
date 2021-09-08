1ab6432ea490c63104ec645a609f2ad764b4514d
Viktor <vvstvr@gmail.com>
2021-09-08 20:27:14 +0300

    ДЗ №2 и ДЗ №3

--- HelloWorld/src/com/max/idea/Main.java
+++ HelloWorld/src/com/max/idea/Main.java
@@ -2,9 +2,20 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
+        // ДЗ №2
         byte a = 127; // Переменная целочисленного типа с наименьшим диапазоном чисел. Присвоено наибольшее возможное значение.
         double b = 34.56; // Переменная типа с плавающей точкой с наибольшим диапазоном чисел. Присвоено произвольное дробное значение.
-        System.out.println("Значение переменной типа 'byte' = " + a +"");
-        System.out.print("Значение переменной типа 'double' = " + b +"");
+
+        //ДЗ №3
+        int [] MyArray = {5, 6, 7, 8, 9};
+        int temp = MyArray[0];
+        int summa;
+        MyArray[0] = MyArray[MyArray.length - 3];
+        MyArray[2] = temp;
+        summa = MyArray[0] + MyArray[2];
+        System.out.println("Первый элемент массива: " + MyArray[0]);
+        System.out.println("Средний элемент массива: " + MyArray[2]);
+        System.out.println("Сумма первого и среднего элементов массива: " + summa);
+
     }
 }
