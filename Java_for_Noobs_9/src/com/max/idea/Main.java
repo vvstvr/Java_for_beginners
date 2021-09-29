993644a65948d9d5b63704979d175da2315b74d2
Viktor <vvstvr@gmail.com>
2021-09-29 21:06:13 +0300

    Задача №9

--- Java_for_Noobs_9/src/com/max/idea/Main.java
+++ Java_for_Noobs_9/src/com/max/idea/Main.java
@@ -1,15 +1,33 @@
 package com.max.idea;
+import java.util.Scanner;
+
+/*
+Задача №9_ Циклы
+Напишите программу:
+1. Пользователь вводит размер массива и данные с клавиатуры в массив типа double.
+2. Посчитайте среднее арифметическое элементов массива.
+3. После этого произведите вывод массива на экран, где каждый элемент массива умножается на среднее арифметическое
+ */
 
 public class Main {
-    public static void main(String args[]) {
-        int x = 9;
-        do {
-            System.out.print("Значение x: " + x);
-            x++;
-            System.out.print("\n");
-        } while (x < 14);
-
-
-
+    public static void main(String[] args) {
+        double avg = 0;
+        double aver = 0;
+        Scanner input = new Scanner(System.in);
+        // Вводим длинну массива
+        System.out.println("Введите длинну массива: ");
+        int sizeM = input.nextInt();
+        double[] array = new double[sizeM];
+        // Вводим элементы массива и считаем среднеарифметическое элементов массива
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < sizeM; i++) {
+            array[i] = input.nextDouble();
+            avg += array[i];
+            aver = avg / sizeM;
+        }
+        // Умнодаем каждый элемент массива на среднеарифметическое элементов массива и выводим на экран.
+        for (double element : array){
+            System.out.println(element * aver);
+        }
     }
 }
